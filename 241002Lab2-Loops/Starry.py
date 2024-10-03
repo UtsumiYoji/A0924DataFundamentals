abcd80020e13ea27082c002715253b1d5783a4ce
YojiUtsumi <cocosuke.pc.one@gmail.com>
2024-10-02 17:56:40 -0700

    fix just use simple print and loop

--- 241002Lab2-Loops/Starry.py
+++ 241002Lab2-Loops/Starry.py
@@ -1,39 +1,69 @@
 def main():
-    LINES = 7
-    
+    # while True:
+    #     lines = input('How many lines do you want to print? :')
+    #     if lines.isdigit():
+    #         lines = int(lines)
+    #         if 0 < lines:
+    #             break
+    #     print('Invalid input. Please type again.')
+    lines = 7
+
+
     # part1
-    for i in range(LINES):
-        print(' ' * (LINES-i-1) + '*' * (2*i+1))
-    print('\n')
-
+    for i in range(lines):
+        for _ in range(lines-1-i):
+            print(' ', end='')
+        for _ in range(1+2*i):
+            print('*', end='')
+        print()
+    
     # part2
-    for i in range(LINES):
-        print('*' * (LINES-i))
-    print('\n')
+    print()
+    for i in range(lines):
+        for _ in range(lines-i):
+            print('*', end='')
+        print()
 
-    # part3
-    for i in range(LINES):
-        if i < LINES//2:
-            print('*' * (i+1))
+    # part3 
+    print()
+    for i in range(lines):
+        if lines//2 > i:
+            for _ in range(i+1):
+                print('*', end='')
         else:
-            print('*' * (LINES-i))
-    print('\n')
+            for _ in range(lines-i):
+                print('*', end='')
+        print()
 
     # part4
-    for i in range(LINES):
-        if i < LINES//2:
-            print(' ' * i + '*' * (LINES-i*2))
+    print()
+    for i in range(lines):
+        if lines//2 > i:
+            for _ in range(i):
+                print(' ', end='')
+            for _ in range(lines-i*2):
+                print('*', end='')
         else:
-            print(' ' * (LINES-i-1) + '*' * (i*2-LINES+2))
-    print('\n')
+            for _ in range(lines-i-1):
+                print(' ', end='')
+            for _ in range(i*2-lines+2):
+                print('*', end='')
+        print()
 
     # part5
-    for i in range(LINES):
-        if i < LINES//2:
-            print(' ' * (3-i) + '*' * (2*i+1))
+    print()
+    for i in range(lines):
+        if lines//2 > i:
+            for _ in range(lines//2-i):
+                print(' ', end='')
+            for _ in range(1+2*i):
+                print('*', end='')
         else:
-            print(' ' * (i-3) + '*' * (2*(LINES-i)-1))
-
+            for _ in range(i-lines//2):
+                print(' ', end='')
+            for _ in range(1+2*(lines-i-1)):
+                print('*', end='')
+        print()
 
 if __name__ == "__main__":
     main()
